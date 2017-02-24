2de5e345a12d0b5239f95cbc19aa831cb18d5bde
restuagungpamuji <restuweb070595@gmail.com>
2017-02-24 13:33:55 +0700

    create laporan module

--- app/Modules/laporan/laplogaktivitas/routes.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-Route::group(['middleware' => 'auth'], function(){
-
-Route::controller('/laporan/laplogaktivitas', 'App\Modules\laporan\laplogaktivitas\Controllers\LaplogaktivitasController');
-
-});
