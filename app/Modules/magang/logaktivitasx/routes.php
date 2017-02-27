5357eeb77709e0c775e19de845f58df15bffee51
restuagungpamuji <restuweb070595@gmail.com>
2017-02-27 13:48:10 +0700

    update magang log aktivitas

--- app/Modules/magang/logaktivitasx/routes.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-Route::group(['middleware' => 'auth'], function(){
-
-Route::controller('/magang/logaktivitas', 'App\Modules\magang\logaktivitas\Controllers\LogaktivitasController');
-
-});
