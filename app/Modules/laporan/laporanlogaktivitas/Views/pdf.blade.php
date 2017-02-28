82478248a4ae825228aa51a3ef7f881926702da6
restuagungpamuji <restuweb070595@gmail.com>
2017-02-28 20:29:57 +0700

    update laporan log pdf

--- app/Modules/laporan/laporanlogaktivitas/Views/pdf.blade.php
+++ app/Modules/laporan/laporanlogaktivitas/Views/pdf.blade.php
@@ -3,8 +3,16 @@
 $data = DB::select("select date_format(tanggal, '%d / %m / %Y') as tanggal, aktivitas, verifikasi, siswa from mg_log_aktivitas where siswa=$siswa ");
 
 FPDF::AddPage();
-FPDF::SetFont('Arial','B',16);
-FPDF::Cell(0,9,"Laporan Log Aktivitas",'0','1','C',false); 	// 0 = margin 0 auto,   padding top-bottom
+FPDF::SetFont('Arial','',10);
+
+
+FPDF::Image('packages/tugumuda/images/dinustek.png',83,10,45,20);
+FPDF::ln(25);
+$alamat = "Jl. Arjuna No. 36 Semarang 50131, Indonesia";
+$kontak = "Tel. (024) 3568492 Fax. (024) 3568490 Email: marketing@dinustech.com";
+FPDF::Cell(0,0,$alamat,'0','1','C',false);
+FPDF::ln(6);
+FPDF::Cell(0,0,$kontak,'0','1','C',false);
 FPDF::SetFont('Arial','B',14);
 FPDF::ln(4);
 
@@ -20,12 +28,13 @@ $siswa 			= $id_siswa->nm_siswa;
 $asal_sekolah 	= $id_siswa->asal_sekolah;
 $jenis_magang 	= $id_magang->nama;
 
-FPDF::Cell(0,5,$siswa,'0','1','C',false);
-FPDF::Ln(4);
-FPDF::Cell(190,0.5,'','0','1','C',true);
-FPDF::Ln(10);
+FPDF::Ln(15);
 
 FPDF::SetFont('Arial','',10);
+FPDF::Cell(22,0,'Nama','0','1','L',false);
+FPDF::Cell(30,0,' : ',0,0,'R');
+FPDF::Cell(100,0,$siswa,0,0,':');
+FPDF::Ln(6);
 FPDF::Cell(22,0,'Asal Sekolah','0','1','L',false);
 FPDF::Cell(30,0,' : ',0,0,'R');
 FPDF::Cell(100,0,$asal_sekolah,0,0,':');
